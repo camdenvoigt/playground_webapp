505da72e3aed471e13d97560178421eeda4ea280
Camden Voigt <camdenvoigt@gmail.com>
2024-03-06 18:11:27 -0800

    Added vue and webpack. Succesffuly bundled some js code

--- server/app.js
+++ server/app.js
@@ -3,8 +3,12 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.static('Hello World!')
 })
+
+app.use(express.static('dist', {
+  index: ['index.html']
+}))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
