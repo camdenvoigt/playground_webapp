94e844030e67021863a976d9548a5899e6dd2ba5
Camden Voigt <camdenvoigt@gmail.com>
2024-03-07 13:55:48 -0800

    Webpack creates html to serve. Html grabs bundled code

--- server/app.js
+++ server/app.js
@@ -1,14 +1,16 @@
 const express = require('express')
+const path = require('path')
 const app = express()
-const port = 3000
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
-  res.static('Hello World!')
+  let options = {
+    root: path.join(__dirname, 'dist', 'html'),
+  }
+  res.sendFile('home.html', options);
 })
-
-app.use(express.static('dist', {
-  index: ['index.html']
-}))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,48 @@
 const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './client/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    home: './client/home/index.js',
+    test: './client/index.js'
   },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'server', 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      // this will apply to both plain `.js` files
+      // AND `<script>` blocks in `.vue` files
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      // this will apply to both plain `.css` files
+      // AND `<style>` blocks in `.vue` files
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin for the magic
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Home',
+      template: './client/index.html',
+      filename: 'html/home.html',
+      chunks: ["home"]
+    }),
+  ]
 };
